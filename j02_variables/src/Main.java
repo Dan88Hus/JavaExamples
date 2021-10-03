1a42f36e1e7e28685f43a9ecc75e424e935c0410
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-10-03 11:46:39 +0300

    init commit3

---
+++ j02_variables/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+    // boolean byte short int long float double char String
+        int x = 123;
+        System.out.println(x);
+    }
+}
