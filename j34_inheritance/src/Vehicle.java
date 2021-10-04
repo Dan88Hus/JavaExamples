ce93b5985625d77f5837014113b2e45666398035
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-10-04 12:45:51 +0300

    init commit14

---
+++ j34_inheritance/src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    double speed;
+    void go(){
+        System.out.println("this vehicle moving");
+    }
+    void stop(){
+        System.out.println("this vehicle stopped");
+    }
+}
